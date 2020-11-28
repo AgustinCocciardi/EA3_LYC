8b0374e459a4a2fd882396d09e0b8b1113a9e5c6
AgustinCocciardi <42738793+AgustinCocciardi@users.noreply.github.com>
2020-11-28 14:40:14 -0300

    Add files via upload

---
+++ GeneraExe.bat
@@ -0,0 +1,14 @@
+C:\FlexBisonC\GnuWin32\bin\flex EA3.l
+pause
+C:\FlexBisonC\GnuWin32\bin\bison -dyv EA3.y
+pause
+C:\FlexBisonC\GnuWin32\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o EA3.exe
+pause
+pause
+EA3.exe test.txt
+del lex.yy.c
+del y.tab.c
+del y.output
+del y.tab.h
+del EA3.exe
+pause
